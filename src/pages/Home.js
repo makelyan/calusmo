5f1ce7e2dee9c82ffc4fe82810683bfd2bdbd85e
= <=>
2024-03-11 19:07:57 -0700

    Contact us page and responsive header :D

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,6 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
-import "../index.css"
+import "../styles/index.css"
 
 export default function() {
     return (
