932c6648911dd472b912107845342827b18a5aa3
MeatJun <meatjun@berkeley.edu>
2024-04-08 18:48:09 -0700

    Changed layout and added footer css file

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,38 +1,29 @@
-import React, { useState } from "react";
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDiscord, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import '../styles/footer.css'
 
 const Footer = () => {
-  const [email, setEmail] = useState('');
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-
-  // Handler for the form submission
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    // Process form data here
-    console.log({ email, firstName, lastName });
-  };
+  const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-light text-center text-lg-start">
+    <footer className="bg-light text-center text-lg-start footer">
         {/* Social icons and copyright section */}
-        <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        <div className="text-center" style={{ backgroundColor: 'rgb(17, 103, 178)'}}>
           <a href="https://discord.gg/XCngeXWusK" rel="noopener noreferrer" target="_blank" className="text-dark me-4">
-            <FontAwesomeIcon icon={faDiscord} />
+            <FontAwesomeIcon className="social-icon" icon={faDiscord} />
           </a>
           <a href="https://www.linkedin.com/company/usmoberkeley/" rel="noopener noreferrer" target="_blank" className="text-dark me-4">
-            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon className="social-icon" icon={faLinkedin} />
           </a>
           <a href="https://www.instagram.com/ucb_usmo/" rel="noopener noreferrer" target="_blank" className="text-dark me-4">
-            <FontAwesomeIcon icon={faInstagram} />
+            <FontAwesomeIcon className="social-icon" icon={faInstagram} />
           </a>
         </div>
 
-        <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)' }}>
-          © Undergraduate Street Medicine Outreach, 2024. All rights reserved.
+        <div className="text-center p-3" style={{ backgroundColor: 'rgb(17, 103, 178)'}}>
+          © Undergraduate Street Medicine Outreach, {currentYear}. All rights reserved.
         </div>
     </footer>
   );
