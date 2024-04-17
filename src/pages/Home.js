6a65721f0218de19b045e2875317e609d6f6d1f1
emi-sakamoto <ejsakamoto3@gmail.com>
2024-04-17 01:24:22 -0700

    adding decal section and functionless button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,21 +14,28 @@ export default function() {
 
                     </div>
                 </div>
-                <div className = "img1">
-                    <img src= "mission_img.jpeg" alt = "this picture didnt load :/"/>
-                </div>
+
                 <div className = "mission">
                     <div className="missiontxt">
                         <h2>OUR MISSION</h2>
                         <p className = "missionp">Our mission is to overcome and mitigate barriers that houseless communities and encampments face in relation to accessing healthcare and other services provided in the Berkeley area.</p>
                     </div>
-                    
+                    <div className = "img1">
+                        <img src= "mission_img.jpeg" alt = "this picture didnt load :/"/>
+                    </div>
                 </div>
 
 
                 <div className = "decal">
-                    <h2>EDUCATION OUTREACH</h2>
-                    <p className = "educationp">USMO offers an educational course to cal students to educate them on the factors and implications surroinding homelessness. Blah blah ask USMO for more infomration</p>
+                    <div className="eduh2">
+                        <h2>EDUCATION OUTREACH</h2>
+                    </div>
+
+                    <div className="txt-and-button">
+                        <p className = "educationp">USMO offers an educational course to cal students to educate them on the factors and implications surroinding homelessness. Blah blah ask USMO for more infomration</p> 
+                        <button type="submit" className="mt-3 btn btn-primary">Learn More</button>
+                    </div>
+                    
                 </div>
            
 
