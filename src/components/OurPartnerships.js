cc089e7fbe9b7fdb8625b5e583598d2c1eba313d
makelyan <maryam.akelyan@gmail.com>
2026-02-14 15:58:15 -0800

    Lazy loading partnership images for faster loading

--- src/components/OurPartnerships.js
+++ src/components/OurPartnerships.js
@@ -1,27 +1,15 @@
-import allSoulsImage from '../images/partnerships/all-souls-episcopal-parish.png';
-import asucLegalClinicImage from '../images/partnerships/asuc-legal-clinic.png';
-import berkeleyNeedImage from '../images/partnerships/berkeley-need.png';
-import eastBaySpcaImage from '../images/partnerships/east-bay-spca.png';
-import foodNotBombsImage from '../images/partnerships/food-not-bombs.png';
-import homelessActionCenterImage from '../images/partnerships/homeless-action-center.png';
-import housingAndEconomicRightsAdvocatesImage from '../images/partnerships/hera.png';
-import ncapdaImage from '../images/partnerships/ncapda.png';
-import studentsVsPandemicsImage from '../images/partnerships/students-vs-pandemics.png';
-import whereDoWeGoImage from '../images/partnerships/where-do-we-go.png';
-import asucImage from '../images/partnerships/asuc.png';
-
 const logos = {
-  "All Souls Episcopal Parish": allSoulsImage,
-  "ASUC Legal Clinic": asucLegalClinicImage,
-  "Berkeley NEED": berkeleyNeedImage,
-  "East Bay SPCA": eastBaySpcaImage,
-  "Food Not Bombs": foodNotBombsImage,
-  "Homeless Action Center": homelessActionCenterImage,
-  "Housing and Economic Rights Advocates": housingAndEconomicRightsAdvocatesImage,
-  "National Coalition Against Prescription Drug Abuse": ncapdaImage,
-  "Students vs. Pandemics": studentsVsPandemicsImage,
-  "Where Do We Go?": whereDoWeGoImage,
-  "ASUC Community Projects": asucImage
+  "All Souls Episcopal Parish": "/images/partnerships/All Souls Episcopal Parish.png",
+  "ASUC Legal Clinic": "/images/partnerships/ASUC Legal Clinic.png",
+  "Berkeley NEED": "/images/partnerships/Berkeley NEED.png",
+  "East Bay SPCA": "/images/partnerships/East Bay SPCA.png",
+  "Food Not Bombs": "/images/partnerships/Food Not Bombs.png",
+  "Homeless Action Center": "/images/partnerships/Homeless Action Center.png",
+  "Housing and Economic Rights Advocates": "/images/partnerships/Housing and Economic Rights Advocates.png",
+  "National Coalition Against Prescription Drug Abuse": "/images/partnerships/National Coalition Against Prescription Drug Abuse.png",
+  "Students vs. Pandemics": "/images/partnerships/Students vs. Pandemics.png",
+  "Where Do We Go?": "/images/partnerships/Where Do We Go.png",
+  "ASUC Community Projects": "/images/partnerships/ASUC Community Projects.png"
 };
 
 export default function OurPartnerships({ name, link }) {
@@ -31,7 +19,7 @@ export default function OurPartnerships({ name, link }) {
     <>
       <div>
         <a href={link} target="_blank" rel="noopener noreferrer">
-          <img src={logoSrc} alt={`Logo for ${name}`} />
+          <img src={logoSrc} loading="lazy" alt={`Logo for ${name}`} />
         </a>
       </div>
     </>
